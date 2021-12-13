4730fad6e0668e2f396810dacb66c26a920f8249
Khaled-Ajaj <83191304+Khaled-Ajaj@users.noreply.github.com>
2021-12-13 14:12:40 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -41,9 +41,14 @@ void delay_fun(int time)
 	
 }
 
-bool joystick_pressed()
+bool joystick1_pressed()
 {
 	return ((GPIOA->IDR & 0x1) == 0x1);
+}
+
+bool joystick2_pressed()
+{
+	return ((GPIOA->IDR & (8U)) == 8U);
 }
 
 int move_right(int curr_pos)
@@ -71,7 +76,7 @@ int hit_ball_left(int curr_pos, volatile int *delay)
 		}
 		return curr_pos;
 	}*/
-	if ( joystick_pressed() && curr_pos == 0)
+	if ( joystick1_pressed() && curr_pos == 0)
 	{
 		for (i = 0; i < 5; i++)
 		{
@@ -80,7 +85,7 @@ int hit_ball_left(int curr_pos, volatile int *delay)
 		*delay -= 1000;
 		}
 	}
-	else if ((!joystick_pressed() && curr_pos == 0))
+	else if ((!joystick1_pressed() && curr_pos == 0))
 	{
 		curr_pos = move_left(curr_pos);
 	}
@@ -93,7 +98,7 @@ int hit_ball_left(int curr_pos, volatile int *delay)
 int hit_ball_right(int curr_pos, volatile int *delay)
 {
 	int i = 0;
-	if (joystick_pressed() && curr_pos == 5)
+	if (joystick2_pressed() && curr_pos == 5)
 	{
 		for (i = 0; i < 5; i++)
 		{
@@ -102,7 +107,7 @@ int hit_ball_right(int curr_pos, volatile int *delay)
 		*delay -= 1000;
 		}
 	}
-	else if ((!joystick_pressed() && curr_pos == 5))
+	else if ((!joystick2_pressed() && curr_pos == 5))
 	{
 		curr_pos = move_right(curr_pos);
 	}
@@ -118,6 +123,11 @@ int player_serve(int player_num, volatile int *delay)
 	{
 		LCD_WriteChar((uint8_t *) "-", 0, 0, 0);
 		pos = 0;
+		while (!joystick1_pressed())
+	{
+		
+	}
+		pos = hit_ball_left(pos, delay);
 		
 	}
 	
@@ -125,31 +135,77 @@ int player_serve(int player_num, volatile int *delay)
 	{
 		LCD_WriteChar((uint8_t *) "-", 0, 0, 5);
 		pos = 5;
-	}
-	while (!joystick_pressed())
+			while (!joystick2_pressed())
 	{
 		
 	}
-		pos = hit_ball_left(pos, delay);
 		pos = hit_ball_right(pos, delay);
+	}
+	
 		return pos;
 	
+}
+
+
+void show_score(int score1, int score2)
+{
+	uint8_t score_p2_string = score2+48;
+	uint8_t score_p1_string = score1+48;
+	LCD_WriteChar((uint8_t *) "", 0, 0, 1);
+	LCD_WriteChar((uint8_t *) "", 0, 0, 3);
+	LCD_WriteChar((uint8_t *) "", 0, 0, 4);
+	LCD_WriteChar(&score_p2_string, 0, 0, 5);
+	LCD_WriteChar((uint8_t *) "", 0, 1, 2);
+	LCD_WriteChar(&score_p1_string, 0, 0, 0);
+	delay_fun(5000000);
+	LCD_Clear();
 	
 	
 }
 
 
+void movingString(uint8_t* str, uint8_t len) {
+	// Scrolls a string across the LCD
+	uint8_t *cur = str; //pointer to the current start of the string
+	uint8_t dot = 0;
+	uint8_t colon = 0;
+	
+	int i;
+	for(i = 0; i < len ;i++) { //loop over entire string
+		int j;
+		for(j = 0; j < 6; j++) { //loop through and print current 6 letters of the string
+			//check to see if next char is a dot or colon
+			if (*cur + i == ':') {
+				colon = 1;
+			}
+			else {
+				colon = 0;
+			}
+			if (*cur + i == '.') {
+				dot = 1;
+			}
+			else {
+				dot = 0;
+			}
+			LCD_WriteChar(cur+j,dot,colon,j); //print character currently being pointed to
+		}
+		cur = str + i; //set current character to the next character in the string
+		delay_fun(500000); //delay between LCD changes
+	}
+}
+
 
 
 int main(void){
 	
-	volatile int ball_pos = -1;
+	volatile int ball_pos = 0;
 	bool start = 0;
 	volatile int time_delay = 300000;
 	volatile int score_p1 = 0;
 	volatile int score_p2 = 0;
-	uint8_t score_p1_string = score_p1+48;
-	uint8_t score_p2_string = score_p2+48;
+	uint8_t *win_msg;
+	//uint8_t score_p1_string = score_p1+48;
+	//uint8_t score_p2_string = score_p2+48;
 	
 	// system clock initialize
 	System_Clock_Init();
@@ -162,19 +218,21 @@ int main(void){
 	RCC->AHB2ENR |=  RCC_AHB2ENR_GPIOAEN;
 	//set to input mode
 	GPIOA->MODER &= ~(3U);
+	GPIOA->MODER &= ~(3U<<6);
 	
-	ball_pos = move_right(ball_pos);
 	LCD_WriteChar((uint8_t *) "-", 0, 0, 0);
-	while (!start)
+	
+	while(1)
 	{
-		if (joystick_pressed())
+		
+		while (!start)
+	{
+		if (joystick1_pressed())
 	{
 		start = 1;
 	}
 		
 	}
-	while(1)
-	{
 
 		ball_pos = hit_ball_left(ball_pos, &time_delay);
 		ball_pos = hit_ball_right(ball_pos, &time_delay);
@@ -182,35 +240,45 @@ int main(void){
 		if(ball_pos == -1)
 		{
 			score_p2++;
-			score_p2_string = score_p2+48;
-			LCD_WriteChar((uint8_t *) "", 0, 0, 1);
-			LCD_WriteChar((uint8_t *) "", 0, 0, 3);
-			LCD_WriteChar((uint8_t *) "", 0, 0, 4);
-			LCD_WriteChar(&score_p2_string, 0, 0, 5);
-			LCD_WriteChar((uint8_t *) "", 0, 1, 2);
-			LCD_WriteChar(&score_p1_string, 0, 0, 0);
-			delay_fun(5000000);
-			LCD_Clear();
+			show_score(score_p1, score_p2);
+			
+			if (score_p2 == 9)
+			{
+				// display win message player 2
+				win_msg = (uint8_t *) "PLAYER 2 WINS. PRESS RESET TO PLAY AGAIN";
+				while(1)
+				{
+				movingString(win_msg, 40);
+				LCD_Clear();
+				}
+				
+			}
+			
+			
 			time_delay = 300000;
 			ball_pos = player_serve(2, &time_delay);
 		}
 		if(ball_pos == 6)
 		{
 			score_p1++;
-			score_p1_string = score_p1+48;
-			LCD_WriteChar(&score_p1_string, 0, 0, 0);
-			LCD_WriteChar((uint8_t *) "", 0, 0, 1);
-			LCD_WriteChar((uint8_t *) "", 0, 0, 3);
-			LCD_WriteChar((uint8_t *) "", 0, 0, 4);
-			LCD_WriteChar((uint8_t *) "", 0, 1, 2);
-			LCD_WriteChar(&score_p2_string, 0, 0, 5);
-			delay_fun(5000000);
-			LCD_Clear();
+			show_score(score_p1, score_p2);
+			
+			if (score_p1 == 9)
+			{
+				// display win message player 1
+				win_msg = (uint8_t *) "PLAYER 1 WINS. PRESS RESET TO PLAY AGAIN";
+				while(1)
+				{
+				movingString(win_msg, 40);
+				LCD_Clear();
+				}
+			}
+			
 			time_delay = 300000;
 			ball_pos = player_serve(1, &time_delay);
 		}
 		
-	
+			
 	}
 
 }
